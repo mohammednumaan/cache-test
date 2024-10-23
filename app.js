b9fd70e7eb5763721d003abfa68354ecd753c202
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2024-10-23 19:25:20 +0530

    feat: Implemented a simple in-memory cache.

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 // imports
 const express = require("express");
-const mongoose = require("mongoose");
+const morgan = require("morgan")
+const Cache = require("./cache");
 
-// initialise express app object
+// initialise express application
 const app = express();
 
+// attaches the morgan middleware to log http requests
+// in the middleware chain
+app.use(morgan(':method :url :response-time'))
+
+// initialize a cache object which takes time (in minutes) 
+// for the cache's ttl
+const cache = new Cache(1);
+
 // a simple route to handle a request in the index route
-app.get('/', (req, res, next) => {
-    console.log("Hello!")
+app.get('/', async (req, res, next) => {
+    try{
+
+        // retrieves the cached data from memory
+        const cachedData = cache.readCache();
+
+        // if there is cached data, we simply
+        // send the cached data to the user
+        if (cachedData){
+            return res.json(cachedData);
+        }
+
+        // if there is no cached data, we simply fetch the data 
+        // and store it in the cache for future requests
+        else{
+            const response = await fetch("https://fakestoreapi.com/products");
+            if (!response.ok){
+                throw new Error(`${response.statusText}`, {cause: {status: response.status, message: response.statusText}});
+            } else{
+                const productData = await response.json();
+                cache.setCache(productData)
+                return res.json({productData});
+            }
+        }
+    } catch(err){
+        return res.status(err.cause.status).json({error: err.cause.message});
+    }
 })
 
 // configuring the express app to listen for
 // requests in port 3000
 app.listen(3000, () => {
-    console.log("Listening At Port: 3000!");
+    console.log("Listening At Port 3000 For Requests...");
 })
---
+++ cache.js
@@ -0,0 +1,39 @@
+// a simple in-memory implementation of the 'cache-aside' caching strategy
+class Cache{
+    constructor(timeToLive = 10){
+        this.cache = null;
+        this.timeToLive = timeToLive * 60 * 1000;
+        this.lastUpdatedAt = Date.now();
+    }
+
+    // a simple private method to check if the cache stored is expired
+    // i.e, the cache's ttl is reached or exceeded
+    #isExpired(){
+        return Date.now() - this.lastUpdatedAt >= this.timeToLive
+    }
+
+    // a simple async method to return the cached data if it exists
+    // if it doesn't, it simply fetches the data and returns it
+    readCache(){
+        if (this.#isExpired() || !this.cache){
+            return null;
+        }
+        return this.cache;
+    }
+
+    // a simple method to set the cache data in 
+    // the cache storage
+    setCache(data){
+        this.cache = data;
+        this.lastUpdatedAt = Date.now();
+    }
+
+    // a simple method that resets the cache 
+    // (forces the cache to expire)
+    resetCache(){
+        this.cache = null;;
+        this.timeToLive = new Date(0);
+    }
+}
+
+module.exports = Cache;
